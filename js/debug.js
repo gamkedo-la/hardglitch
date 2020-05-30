6ca03d2e575c76f387beb958080ed4ade67c74f4
Klaim (A. Joël Lamotte) <mjklaim@gmail.com>
2020-05-30 21:37:04 +0200

    Display mouse position on the game's grid

--- js/debug.js
+++ js/debug.js
@@ -4,6 +4,7 @@ export { setText, setCentralText, display };
 
 import * as graphics from "./system/graphics.js";
 import * as input from "./system/input.js";
+import { current_game_view } from "./main.js";
 
 let text_to_display = "READY";
 let central_text = null;
@@ -19,8 +20,12 @@ function setCentralText(text){ // TODO: add a text display in the Canvas to disp
 }
 
 function display_mouse_position(){
+    if(!current_game_view)
+        return;
+
     const center = graphics.canvas_center_position();
-    graphics.draw_text(`Mouse: X = ${input.mouse.position.x}\tY = ${input.mouse.position.y}`, {x: center.x, y: 20 });
+    const mouse_grid_pos = current_game_view.mouse_grid_position;
+    graphics.draw_text(`Mouse: X = ${input.mouse.position.x}\tY = ${input.mouse.position.y}\t\tGRID: X = ${mouse_grid_pos.x}\tY = ${mouse_grid_pos.y}`, {x: center.x, y: 20 });
 }
 
 function display(){
