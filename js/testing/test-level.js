2bbf7dfdd784199b8fb8218d2d0c11d9bffb7f90
Klaim (A. Joël Lamotte) <mjklaim@gmail.com>
2020-05-30 20:36:00 +0200

    Tweaked test level to check tiling system (all good)

--- js/testing/test-level.js
+++ js/testing/test-level.js
@@ -15,22 +15,42 @@ import { world_grid, default_rules } from "../definitions-world.js";
 
 function make_test_world(){ // The game assets must have been initialized first.
     const test_world_size = { width: 12, height: 12 }; // = world_grid;
-    const world = new concepts.World( test_world_size.width, test_world_size.height );
-    world._floor_tile_grid.set_at({x:5, y:5}, tiles.ID.GROUND);
-    world._floor_tile_grid.set_at({x:4, y:4}, tiles.ID.GROUND);
-    world._surface_tile_grid.set_at({x:6, y:6}, tiles.ID.WALL);
-    world._surface_tile_grid.set_at({x:0, y:0}, tiles.ID.ENTRY);
-    world._surface_tile_grid.set_at({x:11, y:11}, tiles.ID.ENTRY);
-    world._surface_tile_grid.set_at({x:4, y:4}, tiles.ID.ENTRY);
-    world._surface_tile_grid.set_at({x:4, y:8}, tiles.ID.ENTRY);
-    world._surface_tile_grid.set_at({x:8, y:8}, tiles.ID.EXIT);
+    const grid_size = test_world_size.height * test_world_size.width;
+    const floor_tile_grid = new Array(grid_size);
+    floor_tile_grid.fill(tiles.ID.GROUND, grid_size /2, grid_size);
+
+    const surface_tile_grid = new Array(grid_size);
+
+    function set_surface_tile(position, tile_id){
+        surface_tile_grid[index_from_position(test_world_size.width, test_world_size.height, position)] = tile_id;
+    };
+
+    function random_position(){
+        return new concepts.Position(random_int(0, test_world_size.width - 1 ), random_int(0, test_world_size.height - 1 ));
+    }
+
+    set_surface_tile({x:6, y:6}, tiles.ID.WALL);
+    set_surface_tile({x:7, y:6}, tiles.ID.WALL);
+    set_surface_tile({x:6, y:7}, tiles.ID.WALL);
+
+    set_surface_tile({x:2, y:0}, tiles.ID.WALL);
+    set_surface_tile({x:3, y:0}, tiles.ID.WALL);
+    set_surface_tile({x:4, y:0}, tiles.ID.WALL);
+
+    set_surface_tile({x:0, y:0}, tiles.ID.ENTRY);
+    set_surface_tile({x:11, y:11}, tiles.ID.ENTRY);
+    set_surface_tile({x:4, y:4}, tiles.ID.ENTRY);
+    set_surface_tile({x:4, y:8}, tiles.ID.ENTRY);
+    set_surface_tile({x:8, y:8}, tiles.ID.EXIT);
+
+
+    const world = new concepts.World( test_world_size.width, test_world_size.height, floor_tile_grid, surface_tile_grid );
 
     world.set_rules(...default_rules);
 
     for(let i = 0; i < 3; ++i){
         const enemy = new RandomActionEnemy();
-        enemy.position.x = random_int(0, test_world_size.width - 1 );
-        enemy.position.y = random_int(0, test_world_size.height - 1 );
+        enemy.position = random_position();
         world.add(enemy);
     }
 
