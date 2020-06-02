167b7f043f202867ae7d821fed9f21058369a7fe
Klaim (A. Joël Lamotte) <mjklaim@gmail.com>
2020-06-02 22:16:09 +0200

    Test level tiles are even more random.

--- js/testing/test-level.js
+++ js/testing/test-level.js
@@ -16,10 +16,13 @@ import { world_grid, default_rules } from "../definitions-world.js";
 function make_test_world(){ // The game assets must have been initialized first.
     const test_world_size = world_grid;
     const grid_size = test_world_size.height * test_world_size.width;
-    const floor_tile_grid = new Array(grid_size);
-    floor_tile_grid.fill(tiles.ID.GROUND, grid_size /2, grid_size);
 
+    const floor_tile_grid = new Array(grid_size);
     const surface_tile_grid = new Array(grid_size);
+
+    function set_floor_tile(position, tile_id){
+        floor_tile_grid[index_from_position(test_world_size.width, test_world_size.height, position)] = tile_id;
+    };
 
     function set_surface_tile(position, tile_id){
         surface_tile_grid[index_from_position(test_world_size.width, test_world_size.height, position)] = tile_id;
@@ -27,6 +30,10 @@ function make_test_world(){ // The game assets must have been initialized first.
 
     function random_position(){
         return new concepts.Position(random_int(0, test_world_size.width - 1 ), random_int(0, test_world_size.height - 1 ));
+    }
+
+    for(let i = 0; i < grid_size / 0.8; ++ i){
+        set_floor_tile(random_position(), tiles.ID.GROUND);
     }
 
     for(let i = 0; i < grid_size / 5; ++ i){
