72302d389f8d3dacd6ba660c6b3db57fff0c957c
Klaim (A. Joël Lamotte) <mjklaim@gmail.com>
2020-06-28 20:15:59 +0200

    Fixed Jump action names.

--- js/rules/rules-movement.js
+++ js/rules/rules-movement.js
@@ -84,7 +84,7 @@ class Jump extends concepts.Action {
     icon_def = sprite_defs.icon_action_move;
 
     constructor(target){
-        super("jump", `Jump tp ${JSON.stringify(target)}`, target); // TODO: do it properly
+        super(`jump_${target.x}_${target.y}`, `Jump to ${JSON.stringify(target)}`, target); // TODO: do it properly
     }
 
     execute(world, body){
