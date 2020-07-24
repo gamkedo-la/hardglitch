4c419712384cd1d852dc219a1b40dfcb72e1f951
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2020-07-23 23:04:23 -0600

    Added to state code

--- js/game-states.js
+++ js/game-states.js
@@ -5,8 +5,36 @@ export const state = {
 }
 
 export class GameState {
-    constructor(state) {
-        this.state = state;
+    constructor(states, initialState) {
+        this.state = states;
+        this.currentState = states[initialState]
+        this.isActive = true;
+    }
+
+    start() {
+        if (this.currentState != null && this.currentState != undefined) {
+            this.isActive = true;
+        }
+        this.currentState.enter();
+    }
+
+    changeState(newState) {
+        if(this.currentState == newState) return;
+        this.changeState.exit();
+        this.currentState = this.states[newState];
+        this.currentState.enter(this.currentState);
+    }
+
+    update(deltaTime) {
+        if(!this.isActive) return;
+        this.currentState.update(deltaTime);
+        const nextState = this.currentState;
+        if (nextState) this.changeState(nextState);
+    }
+
+    render() {
+        if(!this.isActive) return;
+        this.currentState.render();
     }
 
     getState() {
@@ -16,4 +44,12 @@ export class GameState {
     setState(state) {
         this.state = state;
     }
+}
+
+export class State {
+    constructor() { if (this.constructor === State) throw new Error("Can't instantiate abstract class."); }
+    enter() { if (this.constructor === State) throw new Error("Abstract Method"); }
+    update() { if (this.constructor === State) throw new Error("Abstract Method"); }
+    render() { if (this.constructor === State) throw new Error("Abstract Method"); }
+    exit() { if (this.constructor === State) throw new Error("Abstract Method"); }
 }
