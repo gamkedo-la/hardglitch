d30e8a7645c3505c0c7d854e231e29fa398fe78c
Tylor Allison <tylor@foobox.com>
2020-09-21 09:53:57 -0500

    added test wall definitions

--- js/definitions-tiles.js
+++ js/definitions-tiles.js
@@ -47,6 +47,18 @@ const ID = {
     ASHLVL3B : 108,
     ASHLVL4A : 109,
     ASHLVL4B : 110,
+
+    TYWALL1A: 120,
+    TYWALL1B: 121,
+    TYWALL2A: 122,
+    TYWALL2B: 123,
+    TYWALL3A: 124,
+    TYWALL3B: 125,
+    TYWALL4A: 126,
+    TYWALL4B: 127,
+    ASHWALL3B: 128,
+    ASHWALL4A: 129,
+    ASHWALL4B: 130,
 };
 
 // NOTE: these are filled out by iterating through the definitions below.  if is_surface is included, the tile type is added to surface_tiles, otherwise to floor tiles
@@ -175,6 +187,7 @@ const defs = {
             }
         }
     },
+
     [ID.WALL2] : {
         sprite_def: sprite_defs.wall,
         is_walkable: false,
@@ -422,6 +435,336 @@ const defs = {
         shape_template: "ashlvl4b_template",
         tile_match_predicate: (v) => (v==ID.ASHLVL4B),
         tile_same_predicate: (v) => (v==ID.ASHLVL4B),
+    },
+
+    [ID.TYWALL1A] : {
+        sprite_def: sprite_defs.wall,
+        is_walkable: false,
+        is_view_blocking: true,
+        description: "TYWALL1A",
+        is_wall: true,
+        shape_template: "wall_template",
+        tile_match_predicate: (v) => (v==ID.TYWALL1A),
+        tile_same_predicate: (v) => (v==ID.TYWALL1A),
+        pwall: {
+            width: 16,
+            height: 32,
+            colormap: {
+                [sides.top]:    new Color(245,133,73,.75),
+                [sides.bottom]: new Color(245,133,73,.75),
+                [sides.fl]:     new Color(201,86,29,.75),
+                [sides.front]:  new Color(150,66,21,.75),
+                [sides.fr]:     new Color(112,48,16,.75),
+                [sides.br]:     new Color(201,86,29,.25),
+                [sides.back]:   new Color(150,66,21,.25),
+                [sides.bl]:     new Color(112,48,16,.25),
+                [sides.hlm]:    new Color(222,218,90,.25),
+                [sides.hlM]:    new Color(222,218,90,.85),
+            },
+            highlights : {
+                minor: true,
+            }
+        }
+    },
+
+    [ID.TYWALL1B] : {
+        sprite_def: sprite_defs.wall,
+        is_walkable: false,
+        is_view_blocking: true,
+        description: "TYWALL1B",
+        is_wall: true,
+        shape_template: "wall_template",
+        tile_match_predicate: (v) => (v==ID.TYWALL1B),
+        tile_same_predicate: (v) => (v==ID.TYWALL1B),
+        pwall: {
+            width: 16,
+            height: 32,
+            colormap: {
+                [sides.top]:    new Color(101,111,224,.75),
+                [sides.bottom]: new Color(101,111,224,.75),
+                [sides.fl]:     new Color(74,80,168,.75),
+                [sides.front]:  new Color(45,48,103,.75),
+                [sides.fr]:     new Color(27,29,61,.75),
+                [sides.br]:     new Color(74,80,168,.25),
+                [sides.back]:   new Color(45,48,103,.25),
+                [sides.bl]:     new Color(27,29,61,.25),
+                [sides.hlm]:    new Color(91,230,240,.25),
+                [sides.hlM]:    new Color(91,230,240,.85),
+            },
+            highlights : {
+                minor: true,
+            }
+        }
+    },
+
+    [ID.TYWALL2A] : {
+        sprite_def: sprite_defs.wall,
+        is_walkable: false,
+        is_view_blocking: true,
+        description: "TYWALL2A",
+        is_wall: true,
+        shape_template: "wall_template",
+        tile_match_predicate: (v) => (v==ID.TYWALL2A),
+        tile_same_predicate: (v) => (v==ID.TYWALL2A),
+        pwall: {
+            width: 16,
+            height: 32,
+            colormap: {
+                [sides.top]:    new Color(245,73,173,.75),
+                [sides.bottom]: new Color(245,73,173,.75),
+                [sides.fl]:     new Color(171,26,60,.75),
+                [sides.front]:  new Color(112,16,38,.75),
+                [sides.fr]:     new Color(77,11,27,.75),
+                [sides.br]:     new Color(171,26,60,.25),
+                [sides.back]:   new Color(112,16,38,.25),
+                [sides.bl]:     new Color(77,11,27,.25),
+                [sides.hlm]:    new Color(246,117,255,.25),
+                [sides.hlM]:    new Color(246,117,255,.85),
+            },
+            highlights : {
+                minor: true,
+            }
+        }
+    },
+
+    [ID.TYWALL2B] : {
+        sprite_def: sprite_defs.wall,
+        is_walkable: false,
+        is_view_blocking: true,
+        description: "TYWALL2B",
+        is_wall: true,
+        shape_template: "wall_template",
+        tile_match_predicate: (v) => (v==ID.TYWALL2B),
+        tile_same_predicate: (v) => (v==ID.TYWALL2B),
+        pwall: {
+            width: 16,
+            height: 32,
+            colormap: {
+                [sides.top]:    new Color(61,153,83,.75),
+                [sides.bottom]: new Color(61,153,83,.75),
+                [sides.fl]:     new Color(63,143,81,.75),
+                [sides.front]:  new Color(45,103,59,.75),
+                [sides.fr]:     new Color(30,69,39,.75),
+                [sides.br]:     new Color(63,143,81,.25),
+                [sides.back]:   new Color(45,103,59,.25),
+                [sides.bl]:     new Color(30,69,39,.25),
+                [sides.hlm]:    new Color(255,243,77,.25),
+                [sides.hlM]:    new Color(255,243,77,.85),
+            },
+            highlights : {
+                minor: true,
+            }
+        }
+    },
+
+    [ID.TYWALL3A] : {
+        sprite_def: sprite_defs.wall,
+        is_walkable: false,
+        is_view_blocking: true,
+        description: "TYWALL3A",
+        is_wall: true,
+        shape_template: "wall_template",
+        tile_match_predicate: (v) => (v==ID.TYWALL3A),
+        tile_same_predicate: (v) => (v==ID.TYWALL3A),
+        pwall: {
+            width: 16,
+            height: 32,
+            colormap: {
+                [sides.top]:    new Color(73,151,245,.75),
+                [sides.bottom]: new Color(73,151,245,.75),
+                [sides.fl]:     new Color(25,112,166,.75),
+                [sides.front]:  new Color(14,64,97,.75),
+                [sides.fr]:     new Color(10,43,64,.75),
+                [sides.br]:     new Color(25,112,166,.25),
+                [sides.back]:   new Color(14,64,97,.25),
+                [sides.bl]:     new Color(10,43,64,.25),
+                [sides.hlm]:    new Color(97,250,227,.25),
+                [sides.hlM]:    new Color(97,250,227,.85),
+            },
+            highlights : {
+                minor: true,
+            }
+        }
+    },
+
+    [ID.TYWALL3B] : {
+        sprite_def: sprite_defs.wall,
+        is_walkable: false,
+        is_view_blocking: true,
+        description: "TYWALL3B",
+        is_wall: true,
+        shape_template: "wall_template",
+        tile_match_predicate: (v) => (v==ID.TYWALL3B),
+        tile_same_predicate: (v) => (v==ID.TYWALL3B),
+        pwall: {
+            width: 16,
+            height: 32,
+            colormap: {
+                [sides.top]:    new Color(0,60,218,.75),
+                [sides.bottom]: new Color(0,60,218,.75),
+                [sides.fl]:     new Color(100,83,252,.75),
+                [sides.front]:  new Color(66,53,181,.75),
+                [sides.fr]:     new Color(47,38,128,.75),
+                [sides.br]:     new Color(100,83,252,.25),
+                [sides.back]:   new Color(66,53,181,.25),
+                [sides.bl]:     new Color(47,38,128,.25),
+                [sides.hlm]:    new Color(247,20,255,.25),
+                [sides.hlM]:    new Color(247,20,255,.85),
+            },
+            highlights : {
+                minor: true,
+            }
+        }
+    },
+
+    [ID.TYWALL4A] : {
+        sprite_def: sprite_defs.wall,
+        is_walkable: false,
+        is_view_blocking: true,
+        description: "TYWALL4A",
+        is_wall: true,
+        shape_template: "wall_template",
+        tile_match_predicate: (v) => (v==ID.TYWALL4A),
+        tile_same_predicate: (v) => (v==ID.TYWALL4A),
+        pwall: {
+            width: 16,
+            height: 32,
+            colormap: {
+                [sides.top]:    new Color(73,106,157,.75),
+                [sides.bottom]: new Color(73,106,157,.75),
+                [sides.fl]:     new Color(67,95,186,.75),
+                [sides.front]:  new Color(37,52,105,.75),
+                [sides.fr]:     new Color(24,34,66,.75),
+                [sides.br]:     new Color(67,95,186,.25),
+                [sides.back]:   new Color(37,52,105,.25),
+                [sides.bl]:     new Color(24,34,66,.25),
+                [sides.hlm]:    new Color(110,250,250,.25),
+                [sides.hlM]:    new Color(110,250,250,.85),
+            },
+            highlights : {
+                minor: true,
+            }
+        }
+    },
+
+    [ID.TYWALL4B] : {
+        sprite_def: sprite_defs.wall,
+        is_walkable: false,
+        is_view_blocking: true,
+        description: "TYWALL4B",
+        is_wall: true,
+        shape_template: "wall_template",
+        tile_match_predicate: (v) => (v==ID.TYWALL4B),
+        tile_same_predicate: (v) => (v==ID.TYWALL4B),
+        pwall: {
+            width: 16,
+            height: 32,
+            colormap: {
+                [sides.top]:    new Color(215,88,155,.75),
+                [sides.bottom]: new Color(215,88,155,.75),
+                [sides.fl]:     new Color(67,95,186,.75),
+                [sides.front]:  new Color(37,52,105,.75),
+                [sides.fr]:     new Color(24,34,66,.75),
+                [sides.br]:     new Color(67,95,186,.25),
+                [sides.back]:   new Color(37,52,105,.25),
+                [sides.bl]:     new Color(24,34,66,.25),
+                [sides.hlm]:    new Color(0,222,164,.25),
+                [sides.hlM]:    new Color(0,222,164,.85),
+            },
+            highlights : {
+                minor: true,
+            }
+        }
+    },
+
+    [ID.ASHWALL3B] : {
+        sprite_def: sprite_defs.wall,
+        is_walkable: false,
+        is_view_blocking: true,
+        description: "ASHWALL3B",
+        is_wall: true,
+        shape_template: "wall_template",
+        tile_match_predicate: (v) => (v==ID.ASHWALL3B),
+        tile_same_predicate: (v) => (v==ID.ASHWALL3B),
+        pwall: {
+            width: 16,
+            height: 32,
+            colormap: {
+                [sides.top]:    new Color(253,36,183,.75),
+                [sides.bottom]: new Color(253,36,183,.75),
+                [sides.fl]:     new Color(163,23,119,.75),
+                [sides.front]:  new Color(104,14,75,.75),
+                [sides.fr]:     new Color(69,10,50,.75),
+                [sides.br]:     new Color(163,23,119,.25),
+                [sides.back]:   new Color(104,14,75,.25),
+                [sides.bl]:     new Color(69,10,50,.25),
+                [sides.hlm]:    new Color(255,210,63,.25),
+                [sides.hlM]:    new Color(255,210,63,.85),
+            },
+            highlights : {
+                minor: true,
+            }
+        }
+    },
+
+    [ID.ASHWALL4A] : {
+        sprite_def: sprite_defs.wall,
+        is_walkable: false,
+        is_view_blocking: true,
+        description: "ASHWALL4A",
+        is_wall: true,
+        shape_template: "wall_template",
+        tile_match_predicate: (v) => (v==ID.ASHWALL4A),
+        tile_same_predicate: (v) => (v==ID.ASHWALL4A),
+        pwall: {
+            width: 16,
+            height: 32,
+            colormap: {
+                [sides.top]:    new Color(12,71,103,.75),
+                [sides.bottom]: new Color(12,71,103,.75),
+                [sides.fl]:     new Color(46,31,184,.75),
+                [sides.front]:  new Color(20,13,79,.75),
+                [sides.fr]:     new Color(10,7,41,.75),
+                [sides.br]:     new Color(46,31,184,.25),
+                [sides.back]:   new Color(20,13,79,.25),
+                [sides.bl]:     new Color(10,7,41,.25),
+                [sides.hlm]:    new Color(0,222,164,.25),
+                [sides.hlM]:    new Color(0,222,164,.85),
+            },
+            highlights : {
+                minor: true,
+            }
+        }
+    },
+
+    [ID.ASHWALL4B] : {
+        sprite_def: sprite_defs.wall,
+        is_walkable: false,
+        is_view_blocking: true,
+        description: "ASHWALL4B",
+        is_wall: true,
+        shape_template: "wall_template",
+        tile_match_predicate: (v) => (v==ID.ASHWALL4B),
+        tile_same_predicate: (v) => (v==ID.ASHWALL4B),
+        pwall: {
+            width: 16,
+            height: 32,
+            colormap: {
+                [sides.top]:    new Color(209,17,73,.75),
+                [sides.bottom]: new Color(209,17,73,.75),
+                [sides.fl]:     new Color(22,118,125,.75),
+                [sides.front]:  new Color(15,82,87,.75),
+                [sides.fr]:     new Color(10,53,56,.75),
+                [sides.br]:     new Color(22,118,125,.25),
+                [sides.back]:   new Color(15,82,87,.25),
+                [sides.bl]:     new Color(10,53,56,.25),
+                [sides.hlm]:    new Color(0,222,164,.25),
+                [sides.hlM]:    new Color(0,222,164,.85),
+            },
+            highlights : {
+                minor: true,
+            }
+        }
     },
 
 };
