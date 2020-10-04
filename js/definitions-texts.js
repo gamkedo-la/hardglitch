227a7ede9dfc66d4cced77308db4c67109d6a1ea
Klaim (A. Joël Lamotte) <mjklaim@gmail.com>
2020-10-04 19:03:17 +0200

    Added a text (not visible yet) explaining the basics of the game.

---
+++ js/definitions-texts.js
@@ -0,0 +1,12 @@
+// Put here general texts used in the game.
+// Export the texts so that we can use them in the rest of the code.
+
+
+export const help_info = `How To Play in 5 points:
+1. Your Goal: FIND and REACH THE EXIT of the level.
+2. Actions: Use the action buttons at the bottom of the screen. They only appear when the action is possible.
+3. Movement: Use "WASD" keys, click on green highlighted squares or use the "Move" action button.
+4. Information: Point anything to get info through tooltips or the info-box (bottom right of the screen).
+5. Camera: Drag'n'drop the world to move the camera, or use "IJKL" keys.
+`;
+
