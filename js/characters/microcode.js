2327a885bb5190edeb6378b664b4d32cdb23ff14
Klaim (A. Joël Lamotte) <mjklaim@gmail.com>
2020-12-26 17:59:21 +0100

    MicroCode jumps around sometime.

--- js/characters/microcode.js
+++ js/characters/microcode.js
@@ -7,19 +7,36 @@ import * as concepts from "../core/concepts.js";
 import * as items from "../definitions-items.js";
 import { Character } from "../core/character.js";
 import { sprite_defs } from "../game-assets.js";
-import { auto_newlines, random_sample } from "../system/utility.js";
-import { Jump } from "../rules/rules-movement.js";
-import { select_action_by_type } from "./characters-common.js";
+import { auto_newlines, random_int, random_sample } from "../system/utility.js";
+import { move_towards, select_action_by_type } from "./characters-common.js";
 import { Corrupt } from "../rules/rules-corruption.js";
+import { Jump } from "../rules/rules-movement.js";
 
+function actions_to_jump(){
+    return random_int(10,30);
+}
 
 class Corrupter extends concepts.Actor {
+    actions_until_jump = actions_to_jump();
+
     decide_next_action(world, character, possible_actions){
         debug.assertion(()=>world instanceof concepts.World);
         debug.assertion(()=>character instanceof Character);
         debug.assertion(()=>possible_actions instanceof Object);
 
-        const corrupt = this. _corrupt_randomly(character, possible_actions);
+        if(this.actions_until_jump == 0){
+            this.actions_until_jump = actions_to_jump();
+
+            const jump = this._jump_around(character, possible_actions);
+            if(jump)
+                return jump;
+            else
+                return possible_actions.wait; // Skip the turn if we can't jump now.
+        }
+
+        --this.actions_until_jump;
+
+        const corrupt = this._corrupt_randomly(character, possible_actions);
         if(corrupt)
             return corrupt;
 
@@ -37,6 +54,11 @@ class Corrupter extends concepts.Actor {
 
         const corrupt = select_action_by_type(possible_actions, random_target, Corrupt);
         return corrupt;
+    }
+
+    _jump_around(character, possible_actions){
+        const all_jumps = Object.values(possible_actions).filter(action => action instanceof Jump);
+        return random_sample(all_jumps);
     }
 };
 
