747b4e10cd6149bf473afae217762dae9f3b8e70
Klaim (A. Joël Lamotte) <mjklaim@gmail.com>
2020-12-29 20:38:15 +0100

    Program: avoids anomalies and viruses

--- js/characters/program.js
+++ js/characters/program.js
@@ -2,12 +2,41 @@ export {
     Program,
 }
 
-import { RandomActionSelector } from "./test-enemy.js";
+import * as debug from "../system/debug.js";
+import * as concepts from "../core/concepts.js";
+import * as items from "../definitions-items.js";
 import { Character } from "../core/character.js";
 import { sprite_defs } from "../game-assets.js";
 import { auto_newlines } from "../system/utility.js";
+import { closest_entity, move_away } from "./characters-common.js";
+import { VirusBehavior } from "./virus.js";
+
+class ProgramBehavior extends concepts.Actor {
+    decide_next_action(world, character, possible_actions){
+        debug.assertion(()=>world instanceof concepts.World);
+        debug.assertion(()=>character instanceof Character);
+        debug.assertion(()=>possible_actions instanceof Object);
+
+        const ennemy = this._find_closest_enemy(character, world);
+        if(ennemy instanceof Character){
+
+            const move = move_away(character, possible_actions, ennemy.position);
+            if(move instanceof concepts.Action)
+                return move;
+        }
 
 
+
+        return possible_actions.wait;
+    }
+
+    _find_closest_enemy(character, world){
+        return closest_entity(character, world, entity => entity instanceof Character
+                                                        && (entity.actor instanceof VirusBehavior
+                                                            || entity.is_anomaly));
+    }
+
+}
 
 
 class Program extends Character {
@@ -17,12 +46,24 @@ class Program extends Character {
         }}
     };
 
-    description = auto_newlines("User-space program. Eats memory for no reason.", 35);
+    description = auto_newlines("User-space program. Eats memory for no reason.", 30);
 
     constructor(){
         super("Program", );
-        this.actor = new RandomActionSelector;
-        this.stats.inventory_size.real_value = 1;
+        this.actor = new ProgramBehavior;
+
+        this.stats.inventory_size.real_value = 12;
+        this.stats.activable_items.real_value = 2;
+        this.stats.view_distance.real_value = 5;
+        this.stats.ap_recovery.real_value = 10;
+        this.stats.action_points.real_max = 10;
+        this.stats.action_points.real_value = 10;
+        this.stats.integrity.real_max = 40;
+        this.stats.integrity.real_value = 40;
+
+        this.inventory.add(new items.Item_Copy());
+        this.inventory.add(new items.Item_Merge());
+
     }
 
 };
